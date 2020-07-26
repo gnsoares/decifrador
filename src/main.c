176f05a038bb66222f33192abb8a5edaa9f859e1
Gustavo Soares <gusnsoares@gmail.com>
2020-07-25 22:08:36 -0300

    all tests ok

--- src/main.c
+++ src/main.c
@@ -1,23 +1,29 @@
-/* Tarefa de decifrar uma cifra
- *
- * Dica: veja em referencia/cifrador.c o codigo-fonte do programa
- * que gerou as cifras que voce precisa decifrar!
- */
-
 #include <stdio.h>
+#include <string.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 500
+#define mod(a, b) \
+    ({ __typeof__ (a) _a = (a); \
+        __typeof__ (b) _b = (b); \
+      _a >= 0 ? _a % _b : _a % _b + _b; })
+
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
+const char KEY[] = "SENHASECRETA";
+
 
 int main() {
-  /* Leitura da entrada */
-  fgets(buffer_entrada, TAM_BUFFER, stdin);
+    /* Leitura da entrada */
+    fgets(buffer_entrada, TAM_BUFFER, stdin);
 
+    /* Decifracao */
+    for (int i = 0; buffer_entrada[i] != 0; i++)
+        buffer_saida[i] = buffer_entrada[i] < 'A' || buffer_entrada[i] > 'Z' ?
+            buffer_entrada[i] :
+            'A' + mod(buffer_entrada[i] - KEY[i % strlen(KEY)], 26);
 
+    /* Escrever saida na tela */
+    printf("%s", buffer_saida);
 
-  /* Escrever saida na tela */
-  printf("%s\n", buffer_saida);
-
-  return 0;
+    return 0;
 }
